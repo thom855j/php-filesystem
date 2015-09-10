32540ab54227a7b1d1501702ce188c59d29b59e5
Thomas Elvin <thom855j@cvkweb.dk>
2015-09-10 10:11:35 +0200

    Update Zip.php

--- Zip.php
+++ Zip.php
@@ -1,11 +1,6 @@
 <?php
-namespace thom855j\filesystem;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace thom855j\php_filesystem; 
 
 class Zip
 {
