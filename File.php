84b4adf24c6d5fef91b820276eb6bb630bc9e624
Thomas Elvin <thom855j@cvkweb.dk>
2015-09-10 10:10:14 +0200

    Update File.php

--- File.php
+++ File.php
@@ -1,11 +1,5 @@
 <?php 
-namespace thom855j\filesystem; 
-
-/* 
- * To change this license header, choose License Headers in Project Properties. 
- * To change this template file, choose Tools | Templates 
- * and open the template in the editor. 
- */ 
+namespace thom855j\php_filesystem; 
 
 class File 
 { 
