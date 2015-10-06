c98f561aeeb7b9f5738934dce9b6850d51315d8f
Thomas Elvin <thom855j@cvkweb.dk>
2015-10-06 13:42:56 +0200

    better cache handling
    fixed cache

--- examples/cache.php
+++ examples/cache.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once '../src/Cache.php';
 
+use WebSupportDK\PHPFilesystem\Cache;
+
+$cache = new Cache();
+$cache->setDir('cache');
+$cache->setTime(1800);
+$cache->setExt('html');
+
+$cache->start();
+
+echo 'This is a cache test';
+
+$cache->stop();
--- src/Cache.php
+++ src/Cache.php
@@ -1,74 +1,112 @@
-<?php 
+<?php
+/**
+ * Cache system
+ */
+namespace WebSupportDK\PHPFilesystem;
 
-namespace WebSupportDK\PHPFilesystem; 
+use WebSupportDK\PHPHttp\Url;
 
-class Cache { 
+class Cache
+{
 
-    public static function start() { 
-        //$time_start = microtime(true); 
-        // Settings 
-        $cachedir = PATH_CACHE; // Directory to cache files in (keep outside web root) 
-        $cachetime = CACHE_TIME; // Seconds to cache files for 
-        $cacheext = CACHE_EXT; // Extension to give cached files (usually cache, htm, txt) 
-        // Ignore List 
-        $ignore_list = unserialize(CACHE_IGNORE); 
+	protected $_dir, $_time, $_ext, $_ignoreList;
+	private $_url, $_file;
 
-        // Script 
-        $page = Router::uri(); // Requested page 
-        $cachefile = $cachedir . md5($page) . '.' . $cacheext; // Cache file to either load or create 
-        $ignore_page = false; 
-        for ($i = 0; $i < count($ignore_list); $i++) { 
-            $ignore_page = (strpos($page, $ignore_list[$i]) !== false) ? true : $ignore_page; 
-        } 
-        $cachefile_created = ((file_exists($cachefile)) and ( $ignore_page === false)) ? filemtime($cachefile) : 0; 
-        clearstatcache(); 
+	/**
+	 * Directory to cache files in (keep outside web root) 
+	 */
+	public function setDir($directory)
+	{
 
-        // Show file from cache if still valid 
-        if (time() - $cachetime < $cachefile_created) { 
-            ob_start('ob_gzhandler'); 
-            readfile($cachefile); 
-            //$time_end = microtime(true); 
-            //$time = $time_end - $time_start; 
-            //echo '<!-- generated in ' . $time . ' cached page - '.date('l jS \of F Y h:i:s A', filemtime($cachefile)).', Page : '.$page.' -->'; 
-            ob_end_flush(); 
-            exit(); 
-        } 
+		$this->_dir = $directory;
+	}
 
-        // If we're still here, we need to generate a cache file 
-        //Turn on output buffering with gzip compression. 
-        ob_start('ob_gzhandler'); 
-    } 
+	/**
+	 * Seconds to cache files for 
+	 */
+	public function setTime($time)
+	{
 
-    public static function stop() { 
-        $cachedir = PATH_CACHE; // Directory to cache files in (keep outside web root) 
-        $cacheext = 'html'; // Extension to give cached files (usually cache, htm, txt) 
-        $page = Router::uri(); // Requested page 
-        $cachefile = $cachedir . md5($page) . '.' . $cacheext; // Cache file to either load or create 
-        // Now the script has run, generate a new cache file 
-        $fp = @fopen($cachefile, 'w'); 
+		$this->_time = $time;
+	}
 
-        // save the contents of output buffer to the file 
-        fwrite($fp, ob_get_contents()); 
-        fclose($fp); 
+	/**
+	 * Extension to give cached files (usually cache, htm, txt) 
+	 */
+	public function setExt($extension)
+	{
+		$this->_ext = $extension;
+	}
 
-        ob_end_flush(); 
-    } 
+	/**
+	 * Ignore list of pages NOT to cache
+	 */
+	public function setIgnore($params = array())
+	{
+		$this->_ignoreList = $params;
+	}
 
-    public static function clear() { 
-        // Settings 
-        $cachedir = PATH_CACHE; // Directory to cache files in (keep outside web root) 
+	/**
+	 * Start caching BEFORE script
+	 */
+	public function start()
+	{
+		if (!isset($this->_dir)) {
+			return FALSE;
+		}
+		$this->_url = Url::get(); // Requested page 
+		$this->_file = $this->_dir . md5($this->_url) . '.' . $this->_ext; // Cache file to either load or create 
+		$ignore_page = false;
+		for ($i = 0; $i < count($this->_ignoreList); $i++) {
+			$ignore_page = (strpos($this->_url, $this->_ignoreList[$i]) !== false) ? true : $ignore_page;
+		}
+		$cachefile_created = ((file_exists($this->_file)) and ( $ignore_page === false)) ? filemtime($this->_file) : 0;
+		clearstatcache();
 
-        if ($handle = opendir($cachedir)) { 
-            while (false !== ($file = readdir($handle))) { 
-                if ($file != '.' and $file != '..') { 
-                    //echo $file . ' deleted.<br>'; 
-                    unlink($cachedir . '/' . $file); 
-                } 
-            } 
-            closedir($handle); 
-        } 
+		// Show file from cache if still valid 
+		if (time() - $this->_time < $cachefile_created) {
+			ob_start('ob_gzhandler');
+			readfile($this->_file);
+			//$time_end = microtime(true); 
+			//$time = $time_end - $time_start; 
+			//echo '<!-- generated in ' . $time . ' cached page - '.date('l jS \of F Y h:i:s A', filemtime($cachefile)).', Page : '.$page.' -->'; 
+			ob_end_flush();
+			exit();
+		}
 
-        //curl http://www.your_domain.com/empty_caching.php >/dev/null 2>&1 
-    } 
+		// If we're still here, we need to generate a cache file 
+		//Turn on output buffering with gzip compression. 
+		ob_start('ob_gzhandler');
+	}
 
-} 
+	/**
+	 * Run stop AFTER script
+	 */
+	public function stop()
+	{
+		// Now the script has run, generate a new cache file 
+		$fp = @fopen($this->_file, 'w');
+
+		// save the contents of output buffer to the file 
+		fwrite($fp, ob_get_contents());
+		fclose($fp);
+
+		ob_end_flush();
+	}
+
+	public  function clear()
+	{
+
+		if ($handle = opendir($this->_dir)) {
+			while (false !== ($file = readdir($handle))) {
+				if ($file != '.' and $file != '..') {
+					//echo $file . ' deleted.<br>'; 
+					unlink($this->_dir . '/' . $file);
+				}
+			}
+			closedir($handle);
+		}
+
+		//curl http://www.your_domain.com/empty_caching.php >/dev/null 2>&1 
+	}
+}
